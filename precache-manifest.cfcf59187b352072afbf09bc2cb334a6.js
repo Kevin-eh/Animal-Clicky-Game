fa01844b77ce788ef62f508a702494fb21d0f91e
Kevin-eh <54459190+Kevin-eh@users.noreply.github.com>
2020-02-19 21:46:08 -0800

    Updates

---
+++ precache-manifest.cfcf59187b352072afbf09bc2cb334a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe53e69c4d66e30729ca01eb7358d63",
+    "url": "/Animal-Clicky-Game/index.html"
+  },
+  {
+    "revision": "5a89cb5d46f47fce6c55",
+    "url": "/Animal-Clicky-Game/static/css/main.a6d94f98.chunk.css"
+  },
+  {
+    "revision": "da91d33be44c38586777",
+    "url": "/Animal-Clicky-Game/static/js/2.a2eaa613.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Animal-Clicky-Game/static/js/2.a2eaa613.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a89cb5d46f47fce6c55",
+    "url": "/Animal-Clicky-Game/static/js/main.7ec4ac92.chunk.js"
+  },
+  {
+    "revision": "230729db071b072cb9c8",
+    "url": "/Animal-Clicky-Game/static/js/runtime-main.60fd0972.js"
+  },
+  {
+    "revision": "eda84763578832b22f66cfaacb5a529a",
+    "url": "/Animal-Clicky-Game/static/media/grass.eda84763.png"
+  }
+]);
